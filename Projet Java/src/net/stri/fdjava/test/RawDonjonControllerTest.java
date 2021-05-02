75892662497e0db8a6f0dccb49e7acf6153be43c
Fabien CAYRE <fabiencayre81@gmail.com>
2021-05-02 19:40:58 +0200

    Rename du fichierg

--- Projet Java/src/net/stri/fdjava/test/RawDonjonControllerTest.java
+++ Projet Java/src/net/stri/fdjava/test/RawDonjonControllerTest.java
@@ -2,7 +2,7 @@ package net.stri.fdjava.test;
 
 import net.stri.fdjava.controllers.DonjonController;
 import net.stri.fdjava.models.entity.Heros;
-import net.stri.fdjava.views.DonjonVue;
+import net.stri.fdjava.views.SalleDonjonVue;
 
 /**
  * test du contrôleur de donjon
@@ -15,7 +15,7 @@ public class RawDonjonControllerTest {
 	public static void main(String[] args) {
 		Heros heros = RawCreerHerosControllerTest.creerHeros();
 		DonjonController controleur = new DonjonController(heros);
-		DonjonVue vue = new DonjonVue(controleur);
+		SalleDonjonVue vue = new SalleDonjonVue(controleur);
 		
 		vue.afficherEntree();
 		vue.selectionnerSalle();
